69638a10d1b43aeaf21e184546c4a7a9f702512b
mriquero <mriquero@espol.edu.ec>
2022-06-21 20:59:32 -0500

    Agg de impresion de datarate y cambios en validacion de SNR

--- tallerCDD.py
+++ tallerCDD.py
@@ -1,7 +1,7 @@
 import random as rd
 
 
-####
+#Funcion para toma aleatoria de canales y canales sin traslape en 2.4GHz
 def canalesSinTraslape():
 	canalesAleatorios = []
 	canal1 = rd.randint(1,14)			#numero de canal aleatorio 1
@@ -29,7 +29,11 @@ def canalesSinTraslape():
 			if not(((e>a and e<b) or (f>a and f<b)) or ((e>c and e<d) or (f>c and f<d))):
 				listaNoTraslape.append(i+1)
 				print(i+1)
+	if len(listaNoTraslape)==0:
+		print("No existen canales sin traslape")
 
+
+#Funcion para toma aleatoria de canales y canales sin traslape en 5GHz
 def canalesPara5G():
 	canales = [36,40,44,48,52,56,60,64,100,104,108,112,116,120,124,128,132,136,140,144,149,153,157,161,165]
 	canal1 = rd.choice(canales)
@@ -37,7 +41,13 @@ def canalesPara5G():
 	while canal1==canal2:
 		canal2=rd.choice(canales)
 	print("Canales escogidos para 5G:"+str(canal1)+", "+str(canal2))
+	print("Canales sin traslape: ")
+	for canal in canales:
+		if canal!=canal1 and canal!=canal2:
+			print(canal)
 
+
+#Funcion para calcular SNR
 def calcularSNR():
 	factor = int(input(
 		"Escoja el factor que contribuye al piso de ruido que usted desee:\n1) Bluetooth + hornos microonda en uso\n2) Cables o conectores defectuosos\n3) “Interferencias” por sistemas de radio\nR: "))
@@ -48,6 +58,9 @@ def calcularSNR():
 	snr = aplicacion[app-1]-factorPisoRuido[factor-1]
 	return (snr,app)
 
+
+
+"""#Funcion para establecer para la salida dependiendo del valor de SNR
 def calcularColor(snr,app):
 	valoresSNRRecomendados = [15,25,30]
 	print("El valor SNR es: "+str(snr))
@@ -61,10 +74,43 @@ def calcularColor(snr,app):
 		return snr
 	else:
 		print("El mensaje de alerta es:"+'\033[91m'+"Negacion"+'\033[0m')
+		return snr"""
+
+
+#Funcion para establecer para la salida dependiendo del valor de SNR
+def calcularColor(snr):
+	valoresSNRRecomendados = [15,25,30]
+	print("El valor SNR es: "+str(snr))
+
+	if snr in valoresSNRRecomendados:
+		print("El mensaje de alerta es:"+'\033[1;92m' + " OK "+'\033[1;0m')
+		return snr
+	elif (snr == valoresSNRRecomendados[0]-2) or (snr == valoresSNRRecomendados[1]-2) or (snr == valoresSNRRecomendados[2]-2):
+		print("El mensaje de alerta es:"+'\033[1;93m'+" ADVERTENCIA "+'\033[1;0m')
+		return snr
+	else:
+		print("El mensaje de alerta es:"+'\033[1;91m'+" NEGACION "+'\033[1;0m')
 		return snr
 
-####
 
+#Funcion para asociar el valor de SNR obtenido con el datarate correspondiendte en 2.4GHz
+def calcularDataRate(snr):
+	listaSNR = [(4,6),(6,8),(8,10),(10,12),(12,16),(16,20),(20,21),(21,31)]
+	listasDatarate = ["1Mbps","2Mbps","5.5Mbps","18Mbps","24Mbps","36Mbps","48Mbps","54Mbps"]
+	for i in range(0,8):
+		cota1,cota2=listaSNR[i]
+		if snr>=cota1 and snr<cota2:
+			return listasDatarate[i]
+
+
+#Funcion para asociar el valor de SNR obtenido con el datarate correspondiendte en 5GHz
+def calcularDataRate5G(snr):
+	listaSNR = [(4,6),(6,8),(8,10),(10,12),(12,16),(16,20),(20,21),(21,31)]
+	listasDatarate = ["6Mbps","9Mbps","12Mbps","18Mbps","24Mbps","36Mbps","48Mbps","54Mbps"]
+	for i in range(0,8):
+		cota1,cota2=listaSNR[i]
+		if snr>=cota1 and snr<cota2:
+			return listasDatarate[i]
 
 
 ##Menu principal
@@ -72,15 +118,27 @@ opcion1 = 0
 estandar =0
 while opcion1!=3:
 	opcion1= int(input("*****Estimacion de tasa de datos (data rate) en base al PHY seleccionado***** \nElija la banda que desea utilizar \n1) 2.4GHz \n2) 5GHz \n3) Salir \nR: "))
+	while opcion1!=1 and opcion1!=2 and opcion1!=3:
+		opcion1 = int(input("**La opcion ingresada es incorrecta** \nIngrese una de las opciones validas: "))
+
 	if opcion1==1:
 		canalesSinTraslape()
 		while(estandar not in [1,2]):
 			estandar = int(input("Elija el estandar que desea usar:\n1) 802.11b\n2) 802.11g\nR: "))
 		snr,posVRecomendado = calcularSNR()
-		calcularColor(snr,posVRecomendado)
+		#calcularColor(snr,posVRecomendado)
+		calcularColor(snr)
+		datarate = calcularDataRate(snr)
+		print("El datarate segun el SNR estimado es "+datarate)
+
 	elif opcion1==2:
 		canalesPara5G()
 		print("El estandar a usar es: 802.11a")
 		snr, posVRecomendado = calcularSNR()
-		calcularColor(snr, posVRecomendado)
+		#calcularColor(snr, posVRecomendado)
+		calcularColor(snr)
+		datarate5G = calcularDataRate5G(snr)
+		print("El datarate segun el SNR estimado es "+datarate5G)
+
+
 
